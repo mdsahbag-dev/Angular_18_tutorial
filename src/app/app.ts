55cc60587db3a703174cd982252231f5f0638a05
mdsahbag-dev <mohdsahbag0786@gmail.com>
2026-06-25 23:39:10 +0530

    attribute-directive

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { DataBinding } from './components/data-binding/data-binding';
 import { StructuralDirective } from "./components/structural-directive/structural-directive";
 import { EmployeeList } from "./components/employee-list/employee-list";
 import { AddEmployee } from "./components/add-employee/add-employee";
+import { AttributeDirective } from "./components/attribute-directive/attribute-directive";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DataBinding, StructuralDirective, EmployeeList, AddEmployee],
+  imports: [RouterOutlet, DataBinding, StructuralDirective, EmployeeList, AddEmployee, AttributeDirective],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/components/attribute-directive/attribute-directive.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-attribute-directive',
+  imports: [CommonModule,FormsModule],
+  templateUrl: './attribute-directive.html',
+  styleUrl: './attribute-directive.css',
+})
+export class AttributeDirective {
+
+  div1BgColor: string = 'bg-danger';
+  isDiv2Active: boolean = false;
+  num1:string = '';
+  num2:string = '';
+  isCheckBoxActive: boolean = false ;
+
+  
+   studentList: any[] = [
+    {stdId:23,totalMarks:44, name:'Sameer', city:'Noida', isPersent: true},
+    {stdId:21,totalMarks:84, name:'Anup', city:'Delhi', isPersent: false},
+    {stdId:52,totalMarks:96, name:'Rinku', city:'Pune', isPersent: true},
+    {stdId:33,totalMarks:54, name:'Tom', city:'Delhi', isPersent: false},
+    {stdId:78,totalMarks:74, name:'John', city:'NYC', isPersent: false},
+
+   ]
+
+
+  addRedClass(){
+    this.div1BgColor ='bg-danger'
+  }
+  addBlueClass(){
+    this.div1BgColor = 'bg-primary'
+  }
+  toggleDiv2Class(){
+    this.isDiv2Active = !this.isDiv2Active
+  }
+
+}
